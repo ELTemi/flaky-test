c081b089bb5c4aa26116bb58bbdd5e9e82ba258e
eltemi <ruth_olukunle@yahoo.com>
2021-05-12 18:59:59 -0400

    modified cypress tests

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,16 +1,24 @@
 describe('Sign Up', () => {
-  it('Adds person to course', () => {
+  beforeEach(() => {
     cy.visit('/')
+  })
 
-    cy.get('input[name="name"]')
+  it('Adds person to course', () => {
+    cy.get('div')
+      .should('have.class', 'ui')
+      .and('have.class', 'container')
+
+    cy.get('form').within(() => {
+      cy.get('input[name="name"]')
       .click()
       .type('Some Name')
       .should('have.value', 'Some Name')
 
-    cy.get('input[name="email"]')
+      cy.get('input[name="email"]')
       .click()
       .type('some@email.com')
-
+      .should('have.value', 'some@email.com')
+    
     cy.get('select[name="department"]')
       .select('core')
       .should('have.value', 'core')
@@ -21,8 +29,9 @@ describe('Sign Up', () => {
 
     cy.get('input[type="submit"]')
       .click()
+    })
 
-    cy.get('li')
-      .should('contain', 'Some Name - some@email.com - core - git-it')
+    cy.get('ul').children().should('have.length', 1)
+    cy.get('li').eq(0).should('contain', 'Some Name - some@email.com - core - git-it')
   })
 })
